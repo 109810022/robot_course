de6bda3a5a4d6cf54e07a0c6b04ad85aad16e2d0
cchhii0917 <100888502+109810022@users.noreply.github.com>
2023-11-13 19:40:51 +0800

    新增註解

--- hw2/img_pub.py
+++ hw2/img_pub.py
@@ -4,21 +4,23 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge 
 import cv2 
 
+#建立ImagePublisher物件，作為發送圖片端
 class ImagePublisher(Node):
   def __init__(self):
-    super().__init__('image_publisher')
-    self.publisher_ = self.create_publisher(Image, 'img', 10)
-    self.br = CvBridge()
+    super().__init__('image_publisher') #繼承，初始化
+    self.publisher_ = self.create_publisher(Image, 'img', 10) #建立發送端節點
+    self.br = CvBridge() #建立資料轉換橋梁(因為ROS2及cv2讀取、儲存圖片格式不一樣)
 
-  def pub(self,img_root:str = 'test.jpg'):
-    self.cap = cv2.imread(img_root)
-    self.publisher_.publish(self.br.cv2_to_imgmsg(self.cap))  
+  def pub(self,img_root:str='test.jpg'):#定義物件動作(傳送訊息)
+    #將圖片路徑初始定義為"test.py"
+    self.cap = cv2.imread(img_root) #讀取路徑上的圖片
+    self.publisher_.publish(self.br.cv2_to_imgmsg(self.cap)) #將cv2讀取的圖片轉換成ROS2格式，並發送  
 
 def main(args=None):
-  rclpy.init(args=args)
-  image_publisher = ImagePublisher()
-  image_publisher.pub()
-  rclpy.spin(image_publisher)
+  rclpy.init(args=args)#初始化
+  image_publisher = ImagePublisher() #實體化pub
+  image_publisher.pub() #發送訊息
+  rclpy.spin(image_publisher) #等待發送訊息
   
   
 if __name__ == '__main__':
